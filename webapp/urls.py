ee257951ffc37ba9cdd5f3303b1a8a8ed109753e
majbc1999 <majbc1999@gmail.com>
2021-12-03 17:51:07 +0100

    added basic webapp structure

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('test_map', views.test_map, name='test_map'),
+    path('question_manager', views.question_manager, name='question_manager'),
+    path('add_quiz', views.add_quiz, name='add_quiz')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html", {})
 
-
 def test_map(request):
     return render(request, "test_map.html", {})
+
+def question_manager(request):
+    return render(request, "question_manager.html", {})
+
+def add_quiz(request):
+    return render(request, "add_quiz.html", {})
