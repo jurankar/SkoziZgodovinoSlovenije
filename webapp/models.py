63ac46967558845e6739c4ba33d767d1bb5660ed
majbc1999 <majbc1999@gmail.com>
2021-12-04 14:15:11 +0100

    changed form names

--- webapp/models.py
+++ webapp/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Kviz(models.Model):
+class dbKviz(models.Model):
     id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30, default="Kviz 1")
     author = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     password = models.CharField(max_length=50) # geslo za kviz (da ni vsak kviz dostopen vsakomur)
 
-class Question(models.Model):
+class dbQuestion(models.Model):
     id = models.BigAutoField(primary_key=True)
-    kviz = models.ForeignKey(Kviz, on_delete=models.CASCADE)
+    kviz = models.ForeignKey(dbKviz, on_delete=models.CASCADE)
     number = models.IntegerField() # številka vprašanja znotraj kviza
     ime = models.CharField(max_length=100) # ime artefakta
     opis = models.CharField(max_length=1000)
@@ -21,7 +22,7 @@ class Question(models.Model):
     vprasanja = models.CharField(max_length=1000) # seznam vprašanj, odvisno od tipa
     pravilni_odgovori = models.CharField(max_length=1000) # pravilni odgovori, če obstajajo (true false, izbire,...)
 
-class Odgovor(models.Model):
+class dbAnswer(models.Model):
     id = models.BigAutoField(primary_key=True)
-    vprasanje = models.ForeignKey(Question, on_delete=models.CASCADE)
+    vprasanje = models.ForeignKey(dbQuestion, on_delete=models.CASCADE)
     odgovori = models.CharField(max_length=1000)
