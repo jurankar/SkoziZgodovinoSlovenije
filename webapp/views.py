366c09f1e778c56710669b3a7bbf998fd89b5208
majbc1999 <majbc1999@gmail.com>
2021-12-04 14:14:52 +0100

    inserting and reading a database

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from webapp.forms import Kviz
+from webapp.models import dbKviz
 
 def index(request):
     return render(request, "index.html", {})
@@ -9,7 +12,40 @@ def test_map(request):
     return render(request, "test_map.html", {})
 
 def question_manager(request):
-    return render(request, "question_manager.html", {})
+
+    kvizi = dbKviz.objects.all()
+
+    return render(request, "question_manager.html", {'kvizi': kvizi})
 
 def add_quiz(request):
-    return render(request, "add_quiz.html", {})
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = Kviz(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            dbKviz.objects.create(name=form.cleaned_data['ime'], author=form.cleaned_data['avtor'], password=form.cleaned_data['geslo'])
+            return HttpResponseRedirect('/')
+    else:
+        form=Kviz()
+
+    return render(request, "add_quiz.html", {'form': form})
+
+def add_questions(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = Kviz(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+
+            return HttpResponseRedirect('/')
+    else:
+        form=Kviz()
+
+    return render(request, "add_quiz.html", {'form': form})
+
