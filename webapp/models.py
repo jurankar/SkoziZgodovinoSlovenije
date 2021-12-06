4210123a760c68f75e6cf4720b84a23fd72295ee
majbc1999 <majbc1999@gmail.com>
2021-12-06 12:51:48 +0100

    added foreign key

--- webapp/models.py
+++ webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class dbQuiz(models.Model):
@@ -10,31 +11,24 @@ class dbQuiz(models.Model):
     password = models.CharField(max_length=50) # geslo za kviz (da ni vsak kviz dostopen vsakomur)
 
 class OpisnoModel(models.Model):
+    kviz = models.ForeignKey(dbQuiz, on_delete=CASCADE, null=True)
     opis = models.CharField(max_length=1000)
     slika = models.CharField(max_length=200) # upload jpg-ja
     longitude = models.FloatField() # koordinate za leaflet (bomo spremenili na koncu)
     latitude = models.FloatField()
     vprasanje = models.CharField(max_length=1000) # seznam vprašanj, odvisno od tipa
-
     
 class PravilnoNepravilnoModel(models.Model):
+    kviz = models.ForeignKey(dbQuiz, on_delete=CASCADE, null=True)   
     opis = models.CharField(max_length=1000)
     slika = models.CharField(max_length=200) # upload jpg-ja
     longitude = models.FloatField() # koordinate za leaflet (bomo spremenili na koncu)
     latitude = models.FloatField()
-    trditev1 = models.CharField(max_length=10, default="nan")
-    p1 = models.CharField(max_length=1000, default="nan")
-    trditev2 = models.CharField(max_length=10, default="nan")
-    p2 = models.CharField(max_length=1000)
-    trditev3 = models.CharField(max_length=10, default="nan")
-    p3 = models.CharField(max_length=1000)
-    trditev4 = models.CharField(max_length=10, default="nan")
-    p4 = models.CharField(max_length=1000)
-    trditev5 = models.CharField(max_length=10, default="nan")
-    p5 = models.CharField(max_length=1000)
-
+    vprasanje = models.CharField(max_length=1000, default="nan") # seznam vprašanj
+    pravilni_odgovor = models.CharField(max_length=1000, default="nan") # seznam pravilnegih odgovorov
     
 class IzberiOdgovorModel(models.Model):
+    kviz = models.ForeignKey(dbQuiz, on_delete=CASCADE, null=True)
     opis = models.CharField(max_length=1000)
     slika = models.CharField(max_length=200) # upload jpg-ja
     longitude = models.FloatField() # koordinate za leaflet (bomo spremenili na koncu)
@@ -47,26 +41,8 @@ class IzberiOdgovorModel(models.Model):
     odgovor5 = models.CharField(max_length=100, default="nan")
     pravilni_odgovor = models.IntegerField() # številka pravilnega odgovora
 
-
 class dbAnswer(models.Model):
     id = models.BigAutoField(primary_key=True)
-    #vprasanje = models.ForeignKey(dbQuestion, to_field='id', on_delete=models.CASCADE)
+    #vprasanje = models.ForeignKey(dbQuestion, to_field='id', on_delete=models.CASCADE) # kako foreign key na več kot eno tabelo
     vprasanje = models.CharField(max_length=100)
     odgovori = models.CharField(max_length=1000)
-    
-    
-    
-
-class dbQuestion(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    #kviz = models.ForeignKey(dbQuiz, to_field='id', on_delete=models.CASCADE) # foreign key
-    kviz = models.CharField(max_length=100)
-    number = models.IntegerField() # številka vprašanja znotraj kviza
-    ime = models.CharField(max_length=100) # ime artefakta
-    opis = models.CharField(max_length=1000)
-    slika = models.CharField(max_length=200) # ime jpg datoteke zraven vprašanja
-    longitude = models.FloatField(max_length=10) # koordinate za leaflet
-    latitude = models.FloatField(max_length=10)
-    type = models.CharField(max_length=200)  # tip vprašanja (true-false, izberi pravilni odgovor, opisni tip odgovora, izberi vse pravile odgovore,...)
-    vprasanja = models.CharField(max_length=1000) # seznam vprašanj, odvisno od tipa
-    pravilni_odgovori = models.CharField(max_length=1000) # pravilni odgovori, če obstajajo (true false, izbire,...)
